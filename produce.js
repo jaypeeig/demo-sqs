8c043d32a5a679a3cfb722c5dc41f66423e8f995
Jaypee <jignacio@cambridge.org>
2022-03-28 18:25:25 +0800

    Add producer function

---
+++ produce.js
@@ -0,0 +1,45 @@
+const AWS = require('aws-sdk');
+const {faker} = require('@faker-js/faker');
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials({
+  profile: 'shopfront'
+});
+
+const sqs = new AWS.SQS({
+  apiVersion: '2012-11-05',
+  region: 'ap-southeast-1'
+});
+
+const QueueUrl = 'https://ap-southeast-1.queue.amazonaws.com/362989642060/DemoQueue';
+
+const sendMessage = function () {
+  const params = {
+    DelaySeconds: 2,
+    MessageAttributes: {
+      'Order': {
+        DataType: 'String',
+        StringValue: `Fried ${faker.animal.type()}`
+      },
+      'Customer': {
+        DataType: 'String',
+        StringValue: faker.name.findName()
+      },
+      'Email': {
+        DataType: 'String',
+        StringValue: faker.internet.email()
+      }
+    },
+    MessageBody: `Mang Inasal New Order: ${faker.datatype.uuid()}`,
+    QueueUrl
+  };
+  console.log('Payload', params)
+
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      console.log('Error', err)
+    } else {
+      console.log('Success', data.MessageId)
+    }
+  })
+
+}();
